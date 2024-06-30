7840178e71ef73a08414eeff218747e1176088d2
Ronan Kelly <30551527+rmiguelkelly@users.noreply.github.com>
2024-06-30 01:39:49 -0400

    Add support for CUDA

---
+++ quickcluster/src/quickcluster/device/compute_centroids.cu
@@ -0,0 +1,7 @@
+
+#include <quickcluster/device/common.h>
+
+__device__ void compute_nearest_centroids(const float *data, const float *centroids, DataContext *context, unsigned long *results) {
+
+    
+}
